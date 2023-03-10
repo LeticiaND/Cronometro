370481dfe44f7799641062f33c707abad30fb104
Leticia <leticiandejesus@icloud.com>
2023-03-09 23:12:27 -0300

    Salvando codigo

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const reset = document.querySelector("#reset")
 
 botaoStart.addEventListener("click", ()=> {
     start() // chamando a funcao 
+    milliseconds()
 })
 
 function start() {
@@ -13,9 +14,20 @@ function start() {
     let sec = date.getSeconds() 
     segundos.innerHTML = sec // chamando segundos pois é a posicao onde queremos que funcione, inerhtm para inserir um elemento html ja existente em um documento js
 
+    var timer = setInterval(start,1000);
+ 
+}
+
+function milliseconds(){
+    let date = new Date()
+
     let mile = date.getMilliseconds()
     tens.innerHTML = mile
-    
-}
-var timer = setInterval(start,1000);
 
+    if(mile < 10){
+        tens.innerHTML = "0" + mile
+    }else{
+        tens.innerHTML = mile
+    }
+    var timerr = setInterval(milliseconds,10)
+}
