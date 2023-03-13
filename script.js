9f6c1e893dbd930ab6091dc445271f1cf15eee61
Leticia <leticiandejesus@icloud.com>
2023-03-13 16:03:57 -0300

    salvando codigo

--- script.js
+++ script.js
@@ -1,21 +1,16 @@
-window.onload = function (){
-// criando as variaveis responsaveis pelos 00:00
+window.onload = function (){ 
 
     let seconds = 00
     let tens = 00
-//
+
     const milesemos = document.querySelector("#tens")
     const segundos = document.querySelector("#seconds")
     const botaoStart = document.querySelector("#start")
     const botaoStop = document.querySelector("#stop")
     const botaoReset = document.querySelector("#reset")
- 
-// criando a variavel responsavel que guarda o valor do stop
 
     let Interval
-//
 
-    // criando as funcoes de cada botao
     botaoStart.onclick = function(){
         clearInterval(Interval)
         Interval = setInterval(startTimer, 10)
@@ -33,9 +28,25 @@ window.onload = function (){
         segundos.innerHTML = seconds
     }
     // 
+    function startTimer(){
+        tens++
 
+        if(tens <= 9){
+            milesemos.innerHTML = "0" + tens
+        }
+        if(tens > 9){
+            milesemos.innerHTML = tens
+        }
 
-    function start(){
-        
+        if(tens > 99){
+            console.log("seconds")
+            seconds++
+            segundos.innerHTML = "0" + seconds
+            tens = 0
+            milesemos.innerHTML = "0" + 0
+        }
+        if(seconds > 9){
+            segundos.innerHTML = seconds
+        }
     }
     }
